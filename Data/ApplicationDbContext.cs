692b1a3cf7d3f27804be7c4a49fdb2d30774d357
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2024-03-03 00:18:51 +0100

    initial db setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(_configuration.GetSection("ConnectionStrings")["Default"], new MySqlServerVersion(new Version(8, 0, 36)));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
